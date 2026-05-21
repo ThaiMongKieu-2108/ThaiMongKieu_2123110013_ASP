141b9d8816822fe64ecd5fd2fc120d65f82e7ccb
haru8766 <thaimongkieu229@gmail.com>
2026-05-21 10:59:48 +0700

    hoàn thành bài tập buổi 4

--- CMS.Backend/obj/Debug/net8.0/CMS.Backend.AssemblyInfo.cs
+++ CMS.Backend/obj/Debug/net8.0/CMS.Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMS.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e89aab728e62560ba962d477df52f729fa55258")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9e193c094a43f4dd0e1867d4cc69fb3651c5cee")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMS.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMS.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

1cccc89123f971cc252a696f6b09be043f37fa48
haru8766 <thaimongkieu229@gmail.com>
2026-06-04 09:42:16 +0700

    bổ sung layout đồng bộ

--- CMS.Backend/obj/Debug/net8.0/CMS.Backend.AssemblyInfo.cs
+++ CMS.Backend/obj/Debug/net8.0/CMS.Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMS.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e31a4e62394632df3d957378dd7dced950dea3ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50e435ce32a8db877942275ac1b9a84b59c13170")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMS.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMS.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
